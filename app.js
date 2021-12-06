b9fef15c22662185d2a7cde032b7a3e755204f52
chloemk <minsung.chloe.kim@gmail.com>
2021-12-06 14:48:15 +0900

    Complied file

---
+++ app.js
@@ -0,0 +1,21 @@
+//* object
+var object1 = {
+    x: 1,
+    y: 1
+};
+var object2 = {
+    x: 1,
+    y: 1,
+    z: 1
+};
+//* class
+var Pos1 = /** @class */ (function () {
+    function Pos1() {
+    }
+    return Pos1;
+}());
+var Pos2 = /** @class */ (function () {
+    function Pos2() {
+    }
+    return Pos2;
+}());
